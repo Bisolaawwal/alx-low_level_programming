1ef1c4b2533be7591719bd07d6b98055c7ca18a2
Bisolaawwal <muhammadulawwal06@gmail.com>
2022-03-16 01:37:03 -0700

    bis

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
 */
 void print_alphabet_x10(void)
 {
-int i, a;
+	int i, a;
 
-for (i = 0; i <= 9; i++)
-{
-for (a = 97; a <= 122; a++)
-{
-_putchar(a);
-}
-_putchar('\n');
-}
+		for (i = 0; i <= 9; i++)
+	{
+		for (a = 97; a <= 122; a++)
+	{
+		_putchar(a);
+	}
+		_putchar('\n');
+	}
 }
